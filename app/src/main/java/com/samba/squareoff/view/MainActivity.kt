84c518a89a71043326e28b83f034cf59b05d3bcc
tejHackerDEV <tejasimha222@gmail.com>
2021-07-15 03:02:26 +0530

    ui: Add MainActivity ui
    Add Recycler-View ui for both portrait & landscape
    
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- app/src/main/java/com/samba/squareoff/view/MainActivity.kt
+++ app/src/main/java/com/samba/squareoff/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.samba.squareoff
+package com.samba.squareoff.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.samba.squareoff.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
